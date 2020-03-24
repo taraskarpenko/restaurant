84dfa0fdf98c4063f34c0d3f4c3781bbc084b614
Taras Karpenko <tkarpenko@newstore.com>
2020-03-24 21:05:02 +0100

    app start

--- restaurant/app.py
+++ restaurant/app.py
@@ -1,12 +1,17 @@
 import os
-from flask import Flask, Response, Request
+
+from flask import Flask, Response
+
 app = Flask(__name__)
 
-FLASK_RUN_PORT = int(os.getenv('FLASK_RUN_PORT', '3001'))
+FLASK_RUN_PORT = int(os.getenv('FLASK_RUN_PORT', '5000'))
+FLASK_RUN_HOST = os.getenv('FLASK_RUN_HOST', '127.0.0.1')
+
 
 @app.route('/ping', methods=['GET'])
 def ping():
-    return Response(str({'success':True}), 200, mimetype='application/json')
+    return Response(str({'success': True}), 200, mimetype='application/json')
+
 
 if __name__ == '__main__':
-    app.run(port=FLASK_RUN_PORT)
+    app.run(host=FLASK_RUN_HOST, port=FLASK_RUN_PORT)
---
+++ restaurant/models/restaurant.py
@@ -0,0 +1,3 @@
+class Restaurant():
+    def __init__(self):
+        pass
